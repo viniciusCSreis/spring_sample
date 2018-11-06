b7629c95d7a91077e8efe7cdc649ee8b50ff98f7
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-11-06 11:44:40 -0200

    setter injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,12 @@
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        CustomerService service = new CustomerServiceImpl();
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
