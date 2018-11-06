8069a6f661dc84a9148465866607fbe20ca51b3b
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-11-06 14:10:27 -0200

    autowired

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,13 @@
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        CustomerService service = new CustomerServiceImpl();
 
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -3,12 +3,26 @@ package com.pluralsight.service;
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+
+    private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(){}
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        System.out.println("We are using construtor injection");
+        this.customerRepository = customerRepository;
+    }
+
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll(){
