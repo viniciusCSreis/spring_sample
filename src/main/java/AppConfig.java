ca20c0afe0a631b23d1e46502bf7648139a6a907
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-11-06 15:18:03 -0200

    properties

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,19 @@ import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
+@PropertySource("app.properties")
 public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
 
 //    @Bean( name = "customerService")
 //    public CustomerService getCustomerService(){
--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,8 +10,15 @@ import java.util.List;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
+
     @Override
     public List<Customer> findAll(){
+
+        System.out.println(dbUsername);
+
         List<Customer> customers = new ArrayList();
 
         Customer customer = new Customer();
