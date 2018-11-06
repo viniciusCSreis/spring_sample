df67399170d33aafe6126a85bb1b0defbebf2aaf
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-11-06 13:11:41 -0200

    member injection autowire

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -3,12 +3,16 @@ package com.pluralsight.service;
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Override
     public List<Customer> findAll(){
