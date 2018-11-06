0e12f0fc7936fd888f7479fb20c5c10881e882bc
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-11-06 15:08:09 -0200

    properties

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,14 +1,23 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
+
+
     @Override
     public List<Customer> findAll(){
+
+        System.out.println(dbUsername);
+
         List<Customer> customers = new ArrayList();
 
         Customer customer = new Customer();
