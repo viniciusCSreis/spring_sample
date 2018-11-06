de6ac9717c70de07f007a9887a60a8879a9b6526
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-11-06 14:13:25 -0200

    power of autowired

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -10,17 +10,17 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
-    @Bean( name = "customerService")
-    public CustomerService getCustomerService(){
-        //return new CustomerServiceImpl(getCustomerRepository());
-        CustomerService service = new CustomerServiceImpl();
-        //((CustomerServiceImpl) service).setCustomerRepository(getCustomerRepository());
-        return service;
-    }
-
-    @Bean( name = "customerRepository")
-    public CustomerRepository getCustomerRepository(){
-        return new HibernateCustomerRepositoryImpl();
-    }
+//    @Bean( name = "customerService")
+//    public CustomerService getCustomerService(){
+//        //return new CustomerServiceImpl(getCustomerRepository());
+//        CustomerService service = new CustomerServiceImpl();
+//        //((CustomerServiceImpl) service).setCustomerRepository(getCustomerRepository());
+//        return service;
+//    }
+//
+//    @Bean( name = "customerRepository")
+//    public CustomerRepository getCustomerRepository(){
+//        return new HibernateCustomerRepositoryImpl();
+//    }
 
 }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,11 +2,12 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
 
@@ -21,6 +22,7 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("CustomerRepository set");
         this.customerRepository = customerRepository;
     }
 
